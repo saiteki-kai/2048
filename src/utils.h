7d0b36a863e373b5b29237fc200a9d7a9b5fb972
saiteki-kai <giuseppe.magazu@gmail.com>
2024-11-30 17:20:20 +0100

    style: format

--- src/utils.h
+++ src/utils.h
@@ -13,4 +13,4 @@ void SetRenderColor(SDL_Renderer *renderer, const SDL_Color &color);
 
 void FillRect(SDL_Renderer *renderer, const SDL_FRect *rect, const SDL_Color &color);
 
-void AlignTextRect(SDL_FRect& rect, float text_height, float text_width, TextAlignment alignment);
+void AlignTextRect(SDL_FRect &rect, float text_height, float text_width, TextAlignment alignment);
